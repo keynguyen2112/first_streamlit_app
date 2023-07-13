e54249acc29363e5ebb617e2d93786139f81b02d
keynguyen2112 <139431425+keynguyen2112@users.noreply.github.com>
2023-07-13 15:57:18 +1000

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('Black Pink in your area')
+streamlit.title('Black Pink in your are')
 
-streamlit.text(' Rose')
-streamlit.text('Jennie')
-streamlit.text('Lisa')
-streamlit.text('Jisoo')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
